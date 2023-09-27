63cafb9a293096e608d240b7550941a625f87072
youheng7185 <youheng7185@gmail.com>
2023-09-27 23:46:46 +0800

    wifi changes
    connect wifi screen will close if connected,
    enhance wifi ui

--- main/wifi_connect.cpp
+++ main/wifi_connect.cpp
@@ -10,16 +10,33 @@ void subMenuWiFi() {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Connecting ...");
-    delay(7000); // 7 sec to connect to wifi
+    if (WiFi.status() == WL_CONNECTED) {
+      lcd.clear();
+      lcd.print("Connected");
+      delay(200);
+      returntoHome();
+    }
+    delay(1000); // 1 sec to connect to wifi
     returnMainMenu();
     } else if (WiFi.status() == WL_CONNECTED) {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Wi-Fi connected");
     lcd.setCursor(0, 1);
-    lcd.print("IP address:");
+    lcd.print("SSID:");
+    lcd.setCursor(5, 1);
+    lcd.print(ssid);
     lcd.setCursor(0, 2);
+    lcd.print("IP:");
+    lcd.setCursor(3, 2);
     lcd.print(WiFi.localIP());
+    lcd.setCursor(0, 3);
+    lcd.print("RRSI:");
+    lcd.setCursor(5, 3);
+    lcd.print(WiFi.RSSI());
+    lcd.setCursor(8, 3);
+    lcd.print("dBm");
+    
     delay(200);
     returntoHome();
   }
